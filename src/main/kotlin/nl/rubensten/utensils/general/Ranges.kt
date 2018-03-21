2c78284db46292b9cc737b27192f9643b62610ab
Sten Wessel <sten.wessel@gmail.com>
2018-03-21 15:21:31 +0100

    Migrated to Maven configuration

--- src/main/kotlin/nl/rubensten/utensils/general/Ranges.kt
+++ src/main/kotlin/nl/rubensten/utensils/general/Ranges.kt
@@ -6,3 +6,37 @@ package nl.rubensten.utensils.general
  */
 val IntRange.length
     get() = endInclusive - start + 1
+
+/**
+ * Find the smallest number _n_ in the range for which _[comparator]__(n) <= [element]_.
+ *
+ * The function [comparator] must be increasing.
+ *
+ * @author Sten Wessel
+ */
+fun <T : Comparable<T>> IntRange.lowerBinarySearchBy(element: T, comparator: (Int) -> T): Int {
+    var lower = this.first
+    var upper = this.last
+
+    var estimation: T
+
+    while (lower < upper) {
+        val middle = lower + (upper - lower) / 2
+        estimation = comparator(middle)
+
+        if (estimation > element) {
+            upper = if (upper == middle) upper - 1 else middle
+        }
+        else {
+            lower = if (lower == middle) lower + 1 else middle
+        }
+    }
+
+    // Check if the correct point is found
+    estimation = comparator(lower)
+    while (estimation > element) {
+        estimation = comparator(--lower)
+    }
+
+    return lower
+}
