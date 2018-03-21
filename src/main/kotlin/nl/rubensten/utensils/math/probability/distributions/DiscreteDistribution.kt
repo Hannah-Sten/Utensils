48978d7b64a959cd06e4f1e53e49d0a201520bdf
Sten Wessel <sten.wessel@gmail.com>
2018-03-21 15:45:42 +0100

    Simplified discrete distribution bounds

--- src/main/kotlin/nl/rubensten/utensils/math/probability/distributions/DiscreteDistribution.kt
+++ src/main/kotlin/nl/rubensten/utensils/math/probability/distributions/DiscreteDistribution.kt
@@ -9,11 +9,11 @@ import nl.rubensten.utensils.general.lowerBinarySearchBy
  */
 interface DiscreteDistribution : Distribution<Int> {
 
-    val supportLowerBound: Int?
-        get() = null
+    val supportLowerBound: Int
+        get() = Int.MIN_VALUE
 
-    val supportUpperBound: Int?
-        get() = null
+    val supportUpperBound: Int
+        get() = Int.MAX_VALUE
 
     /**
      * Returns the value of the mass function (PMF) at [x].
@@ -24,30 +24,26 @@ interface DiscreteDistribution : Distribution<Int> {
     fun mass(x: Int): Double
 
     override fun cumulativeProbability(x: Int): Double {
-        if (supportLowerBound?.let { x <= it } == true) return 0.0
-        if (supportUpperBound?.let { x >= it } == true) return 1.0
+        if (x <= supportLowerBound) return 0.0
+        if (x >= supportUpperBound) return 1.0
 
-        if (supportLowerBound == null && supportUpperBound == null)
-            throw NotImplementedError("Not implemented for unbounded support intervals.")
-
-        if (supportLowerBound != null) {
-            if (supportUpperBound?.let { it - x <= supportLowerBound!! - x } == true) {
-                return 1 - (x..supportUpperBound!!).sumByDouble { mass(it) }
-            }
-            return (supportLowerBound!!..x).sumByDouble { mass(it) }
+        // Check whether summing form the right side is less expensive
+        if (supportUpperBound - x < x - supportLowerBound) {
+            return 1 - (x..supportUpperBound).sumByDouble { mass(it) }
         }
 
-        return 1 - (x..supportUpperBound!!).sumByDouble { mass(it) }
+        return (supportLowerBound..x).sumByDouble { mass(it) }
+
     }
 
     override fun inverseCumulativeProbability(p: Double): Int {
         require(p in 0.0..1.0) { "Argument must be a probability." }
 
-        if (p == 0.0) return supportLowerBound ?: Int.MIN_VALUE
-        if (p == 1.0) return supportUpperBound ?: Int.MAX_VALUE
+        if (p == 0.0) return supportLowerBound
+        if (p == 1.0) return supportUpperBound
 
-        val lower = supportLowerBound?.takeIf { it >= Int.MIN_VALUE / 2 + 1 } ?: (Int.MIN_VALUE / 2 + 1)
-        val upper = supportUpperBound?.takeIf { it <= Int.MAX_VALUE / 2 } ?: (Int.MAX_VALUE / 2)
+        val lower = supportLowerBound.takeIf { it >= Int.MIN_VALUE / 2 + 1 } ?: (Int.MIN_VALUE / 2 + 1)
+        val upper = supportUpperBound.takeIf { it <= Int.MAX_VALUE / 2 } ?: (Int.MAX_VALUE / 2)
 
         return (lower..upper).lowerBinarySearchBy(p) { x -> cumulativeProbability(x) }
     }
